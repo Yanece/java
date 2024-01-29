5500391bf598fb3eedef0c871acdfe6da93fa748
Yane_kouame <yane643@gmail.com>
2024-01-29 09:51:50 +0100

    phase de test

---
+++ script.js
@@ -0,0 +1,53 @@
+function trouverIntersection() {
+
+    let tableau1 = document.getElementById('table1');
+    let tableau2 = document.getElementById('table2');
+
+
+    let lignesTableau1 = tableau1.getElementsByTagName('tr');
+    let lignesTableau2 = tableau2.getElementsByTagName('tr');
+
+    let resultats = [];
+
+
+    for (let i = 0; i < lignesTableau1.length; i++) {
+        for (let j = 0; j < lignesTableau2.length; j++) {
+
+        let cellulesTableau1 = lignesTableau1[i].getElementsByTagName('td');
+        let cellulesTableau2 = lignesTableau2[j].getElementsByTagName('td');
+
+
+        if (cellulesTableau1[0].innerText === cellulesTableau2[0].innerText) {
+        resultats.push(cellulesTableau1[0].innerText);
+        }
+    }
+    }
+
+
+    afficherResultat(resultats);
+}
+
+function afficherResultat(resultats) {
+    let resultatDiv = document.getElementById('resultat');
+    resultatDiv.innerHTML = '';
+
+    if (resultats.length > 0) {
+    let tableauResultat = document.createElement('table');
+        let ligne = tableauResultat.insertRow();
+
+
+        let cellule = ligne.insertCell();
+        cellule.innerHTML = '<b>Valeurs Intersection</b>';
+
+
+        for (let i = 0; i < resultats.length; i++) {
+        ligne = tableauResultat.insertRow();
+        cellule = ligne.insertCell();
+        cellule.innerHTML = resultats[i];
+    }
+
+        resultatDiv.appendChild(tableauResultat);
+    } else {
+        resultatDiv.innerHTML = 'Aucune valeur d\'intersection trouvée.';
+    }
+}
